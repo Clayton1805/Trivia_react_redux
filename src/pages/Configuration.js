77a46041e54ee8e9c23de048e6df344e915eb5bb
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-09-19 15:31:47 -0300

    comentando page config para nao quebrar e praarando git rub pages

--- src/pages/Configuration.js
+++ src/pages/Configuration.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import { apiCategory } from '../services/request';
+// import React, { Component } from 'react';
+// import { apiCategory } from '../services/request';
 
-class Configuration extends Component {
-  constructor() {
-    super();
-    this.state = {
-      categories: [],
-    };
-  }
+// class Configuration extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       categories: [],
+//     };
+//   }
 
-  async categories() {
-    const categories1 = await apiCategory();
-    return categories1;
-  }
+//   async categories() {
+//     const categories1 = await apiCategory();
+//     return categories1;
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1 data-testid="settings-title">
-          Configuration
-        </h1>
-        <label>
-          Categoria:
-          <select>
-            {categories2
-              .map(({ id, name }) => <option key={ id }value={ id }>{ name }</option>)}
-          </select>
-        </label>
-        <label>
-          Dificuldade:
-          <select>
-            <option></option>
-          </select>
-        </label>
-        <label>
-          Tipo:
-          <select>
-            <option></option>
-          </select>
-        </label>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h1 data-testid="settings-title">
+//           Configuration
+//         </h1>
+//         <label>
+//           Categoria:
+//           <select>
+//             {categories2
+//               .map(({ id, name }) => <option key={ id }value={ id }>{ name }</option>)}
+//           </select>
+//         </label>
+//         <label>
+//           Dificuldade:
+//           <select>
+//             <option></option>
+//           </select>
+//         </label>
+//         <label>
+//           Tipo:
+//           <select>
+//             <option></option>
+//           </select>
+//         </label>
+//       </div>
+//     );
+//   }
+// }
 
-export default Configuration;
+// export default Configuration;
